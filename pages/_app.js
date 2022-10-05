1003531d104c58d694660caa944d606ff2b148fb
Vcode7 <vikassirvi656@gmail.com>
2022-10-05 10:03:15 +0530

    template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import Head from 'next/head';
+import { Footer } from './components/Footer';
+import { Nav } from './components/Nav';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </Head>
+       
+    <Nav />
+    <Component {...pageProps} />
+    <Footer />
+    </>
+  )
 }
 
 export default MyApp
