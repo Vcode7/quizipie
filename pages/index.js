442624c463d852e20ef2896cf38a83b71309636a
Vikas <90235735+Vcode7@users.noreply.github.com>
2023-08-06 12:00:49 +0530

    adsenseadded

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 
 import Stater from './components/Stater';
-
+import Head from 'next/head'
 
 export default function Home({setSpin}) {
   return (
     <> 
+    <Head >
+      <title>quizepie</title>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3839335974967921"
+     crossorigin="anonymous"></script>
+    </Head>
         <Stater setSpin={setSpin} />         
     </>
   )
